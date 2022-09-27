2cddc11e1a9578d022c21f99c60da21d134a682e
Emanukim <emmanue.kimweli@student.moringaschool.com>
2022-09-27 15:48:29 +0300

    completed lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,16 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  // const linksElements = links.map((link) => {
+  // return <a href="link" key={link}>{link}</a>;
+  // });
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+      {/* {linksElements} */}
+      <a href="#home" key={links[0]} >home</a>
+      <a href="#about" key={links[1]}  >about</a>
+      <a href="#projects" key={links[2]}  >projects</a>
+  </nav>;
 }
 
 export default NavBar;
